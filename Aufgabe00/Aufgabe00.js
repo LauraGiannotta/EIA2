66fccb71938388b70cfec63247d189852a590f8b
lagia <lagia@LAPTOP-8U4NVVVF.eduroam.hs-furtwangen.de>
2018-03-22 14:32:18 +0100

    j

--- Aufgabe00/Aufgabe00.js
+++ Aufgabe00/Aufgabe00.js
@@ -2,7 +2,7 @@ function Name() {
     var feld = prompt("Hallo, wie hei�t du?", "");
     if (feld != null) {
         document.getElementById("GibNameEin").innerHTML =
-            "Sch�ner Name, " + feld + ". Deine Daten wurden erfolgreich an die NSA weitergeleitet.";
+            "Sch�ner Name, " + feld + "!";
     }
 }
 //# sourceMappingURL=Aufgabe00.js.map
--- Aufgabe00/Aufgabe00.ts
+++ Aufgabe00/Aufgabe00.ts
@@ -2,7 +2,7 @@ function Name(): void {
   var feld: string = prompt("Hallo, wie heißt du?", "");
   if (feld != null) {
     document.getElementById("GibNameEin").innerHTML =
-      "Schöner Name, " + feld + ". Deine Daten wurden erfolgreich an die NSA weitergeleitet.";
+      "Schöner Name, " + feld + "!";
   }
 }
 
