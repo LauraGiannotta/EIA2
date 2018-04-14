494f4ccb5993113e527482ba7a82dc46a93dc288
lagia <lagia@LAPTOP-8U4NVVVF>
2018-04-14 21:41:21 +0200

    F

--- Aufg2/memory.js
+++ Aufg2/memory.js
@@ -5,10 +5,17 @@ var Memory;
     let n;
     let cardContent = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",];
     let cardArray;
-    //Eingabe numPlayers 
+    //Eingabe numPlayers und numPairs
     let inputPlayers = prompt("Wieviele Spieler von 1-4?", "");
-    let inputPlayersNum = parseInt(inputPlayers);
-    let inputPairs = prompt("Wieviele Kartenpaare von 10-20?", "");
-    let inputPairsNum = parseInt(inputPairs);
+    let inputNumPlayer = parseInt(inputPlayers);
+    let inputPairs = prompt("Wieviele Kartenpaare von 5-10?", "");
+    let inputNumPairs = parseInt(inputPairs);
+    //createCard Funktion
+    function createCrads(_numPairsFinal) {
+        for (let i = 0; i > _numPairsFinal; i++) {
+            cardArray.splice(0, 0, cardContent[i]);
+            cardArray.splice(1, 0, cardContent[i]);
+        }
+    }
 })(Memory || (Memory = {}));
 //# sourceMappingURL=memory.js.map
--- Aufg2/memory.ts
+++ Aufg2/memory.ts
@@ -6,12 +6,23 @@ namespace Memory {
    let cardContent: string [] = ["A","B","C","D","E","F", "G", "H", "I", "J",];
    let cardArray : string [];
      
-    //Eingabe numPlayers 
+    //Eingabe numPlayers und numPairs
     
       let inputPlayers : string = prompt ("Wieviele Spieler von 1-4?", "");
-      let inputPlayersNum : number = parseInt (inputPlayers);
+      let inputNumPlayer : number = parseInt (inputPlayers);
       
-      let inputPairs : string = prompt ("Wieviele Kartenpaare von 10-20?", "");
-      let inputPairsNum : number = parseInt (inputPairs);
+      let inputPairs : string = prompt ("Wieviele Kartenpaare von 5-10?", "");
+      let inputNumPairs : number = parseInt (inputPairs);
+       
 
+    
+
+    //createCard Funktion
+        
+        function createCrads(_numPairsFinal: number): void {
+        for (let i: number = 0; i > _numPairsFinal; i++) {
+            cardArray.splice(0, 0, cardContent[i]);
+            cardArray.splice(1, 0, cardContent[i]);
+}
+}
 }
