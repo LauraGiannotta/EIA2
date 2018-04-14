c28f7e0cc3b8f1ae6b6d8d28cd768da1abe0b5e3
lagia <lagia@LAPTOP-8U4NVVVF>
2018-04-14 22:40:30 +0200

    vX

--- Aufg2/memory.js
+++ Aufg2/memory.js
@@ -1,38 +1,58 @@
-var Memory;
-(function (Memory) {
-    let numPlayers;
-    let numPairs;
-    let n;
-    let cardContent = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",];
-    let cardArray;
-    //Eingabe numPlayers und numPairs
-    let inputPlayers = prompt("Wieviele Spieler von 1-4?", "");
-    let inputNumPlayer = parseInt(inputPlayers);
-    let inputPairs = prompt("Wieviele Kartenpaare von 5-10?", "");
-    let inputNumPairs = parseInt(inputPairs);
-    //createCard Funktion
+var Aufgabe02;
+(function (Aufgabe02) {
+    var infolist = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var cardList = [];
+    function inputPairs() {
+        let pairs = prompt("Wie viele Kartenpaare (min4 & max26)");
+        let pairSum = parseInt(pairs);
+        console.log("inputPairs");
+        return pairSum;
+    }
+    let amount = inputPairs();
+    console.log("L�nge Infolist", infolist.length);
+    console.log("Content Infolist", infolist);
+    function shuffelAray(x) {
+        for (let i = 1; i <= x; i++) {
+            var content = infolist[0];
+            cardList.push(content);
+            cardList.push(content);
+            var removed = infolist.splice(0, 1);
+        }
+        console.log("Content CardList", cardList);
+    }
     function generateCards(y) {
-        var node = document.getElementById("gamefield");
+        var node = document.getElementById("spielfeld");
         var childNodeHTML;
         var i = 0;
         while (i < (y * 2)) {
             let min = 0;
-            let max = (cardArray.length);
+            let max = (cardList.length);
+            // min = Math.ceil(min);
+            //  max = Math.floor(max);
             var random = Math.floor(Math.random() * (max - min)) + min;
             console.log("Card:" + i);
             console.log(random);
             childNodeHTML = "<div  class='card' id='Karte" + i + "'>";
             childNodeHTML += "<p>";
-            childNodeHTML += cardArray[random];
+            childNodeHTML += cardList[random];
             childNodeHTML += "</p>";
             childNodeHTML += " </div> ";
             node.innerHTML += childNodeHTML;
-            console.log("L�nge CardArray nach Generate, " + cardArray.length);
-            var content = cardArray[random];
-            var removed = cardArray.splice(random, 1);
-            console.log(cardArray);
+            console.log("L�nge Cardlist nach Generate, " + cardList.length);
+            var content = cardList[random];
+            var removed = cardList.splice(random, 1);
+            console.log(cardList);
             i++;
         }
     }
-})(Memory || (Memory = {}));
+    // Hauptprogramm
+    function main() {
+        shuffelAray(amount);
+        console.log("main");
+        // Content erzeugen
+        generateCards(amount);
+    }
+    // Add EventListener 
+    document.addEventListener('DOMContentLoaded', main);
+})(Aufgabe02 || (Aufgabe02 = {}));
 //# sourceMappingURL=memory.js.map
--- Aufg2/memory.ts
+++ Aufg2/memory.ts
@@ -1,49 +1,105 @@
-namespace Memory {
+namespace Aufgabe02 {
+    
 
-   let numPlayers: number;
-   let numPairs: number;
-   let n: number;
-   let cardContent: string [] = ["A","B","C","D","E","F", "G", "H", "I", "J",];
-   let cardArray : string [];
-     
-    //Eingabe numPlayers und numPairs
     
-      let inputPlayers : string = prompt ("Wieviele Spieler von 1-4?", "");
-      let inputNumPlayer : number = parseInt (inputPlayers);
-      
-      let inputPairs : string = prompt ("Wieviele Kartenpaare von 5-10?", "");
-      let inputNumPairs : number = parseInt (inputPairs);
+    var infolist: string []=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var cardList: string[]=[];
+  
+    
+    
+   function inputPairs() : number{
        
-    
-    //createCard Funktion
+    let pairs : string = prompt("Wie viele Kartenpaare (min4 & max26)")
+    let pairSum : number = parseInt(pairs)
+       
+       
+      
+       console.log("inputPairs")
         
-   function generateCards( y:number) : void {
-        var node : any= document.getElementById("gamefield");
+    
+       return pairSum;
+    }
+       
+   let amount :number =  inputPairs();
+   
+        
+    console.log("Länge Infolist", infolist.length);
+    console.log("Content Infolist", infolist);
+    
+    
+    
+    
+    function shuffelAray(x:number): void {
+        
+        for (let i: number = 1; i <= x; i++) {
+             
+            
+        var content : string = infolist[0];  
+        cardList.push (content);
+            cardList.push(content);
+             
+            var removed = infolist.splice(0, 1);
+            
+            
+            
+        }
+    console.log("Content CardList", cardList);
+   
+    
+    }
+    
+    
+    
+    function generateCards(y:number) : void{
+        var node : any= document.getElementById("spielfeld");
         var childNodeHTML : string;
         var i : number = 0;
         
         
         while (i < (y*2)) {
             let min: number = 0;
-        let max: number = (cardArray.length);
-
+        let max: number = (cardList.length);
+            // min = Math.ceil(min);
+          //  max = Math.floor(max);
             var random:number=Math.floor(Math.random() * (max - min)) + min; 
             console.log("Card:" + i);
               console.log(random); 
           
             childNodeHTML = "<div  class='card' id='Karte" + i + "'>";
             childNodeHTML += "<p>";
-            childNodeHTML += cardArray[random];
+            childNodeHTML += cardList[random];
             childNodeHTML += "</p>";
             childNodeHTML += " </div> ";      
             node.innerHTML += childNodeHTML;
-            console.log("Länge CardArray nach Generate, " + cardArray.length)
-            var content : string = cardArray[random];  
+            console.log("Länge Cardlist nach Generate, " + cardList.length)
+            var content : string = cardList[random];  
              
-            var removed = cardArray.splice(random, 1);
-            console.log(cardArray);
+            var removed = cardList.splice(random, 1);
+            console.log(cardList);
             
             i++;
         }
-}
+    }
+    
+    
+    // Hauptprogramm
+    function main () : void {
+        
+         shuffelAray(amount);
+        
+       
+        
+        
+        console.log("main");
+        
+        // Content erzeugen
+         generateCards(amount);
+        }
+    
+    
+    
+    
+    
+    // Add EventListener 
+    document.addEventListener('DOMContentLoaded', main); 
 }
