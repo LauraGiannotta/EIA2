f5beb6506a2f93086086e93a886d1e019e0ce15f
lagia <lagia@LAPTOP-8U4NVVVF>
2018-04-14 21:08:39 +0200

    Test

---
+++ Aufg2/memory.js
@@ -0,0 +1,13 @@
+var Memory;
+(function (Memory) {
+    let numPlayers;
+    let numPairs;
+    let n;
+    let cardContent = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",];
+    let cardArray;
+    //Eingabe numPlayers 
+    function inputPlayers() {
+        var feld = prompt("Wieviele Spieler? (max.4)", "");
+    }
+})(Memory || (Memory = {}));
+//# sourceMappingURL=memory.js.map
---
+++ Aufg2/memory.ts
@@ -0,0 +1,16 @@
+namespace Memory {
+
+   let numPlayers: number;
+   let numPairs: number;
+   let n: number;
+   let cardContent: string [] = ["A","B","C","D","E","F", "G", "H", "I", "J",];
+   let cardArray : string [];
+     
+    //Eingabe numPlayers 
+    
+    function inputPlayers(): void {
+        var feld: string = prompt("Wieviele Spieler? (max.4)", "");
+  
+    }
+
+}
