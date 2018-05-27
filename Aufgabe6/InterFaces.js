f87e1d274c43340227e34a96098ff6be1f551cfa
lagia <lagia@LAPTOP-8U4NVVVF>
2018-05-27 13:15:40 +0200

    Aufgabe6

--- Aufgabe6/InterFaces.js
+++ Aufgabe6/InterFaces.js
@@ -1,1 +1,6 @@
+var Aufgabe6;
+(function (Aufgabe6) {
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    Aufgabe6.studiHomoAssoc = {};
+})(Aufgabe6 || (Aufgabe6 = {}));
 //# sourceMappingURL=InterFaces.js.map
