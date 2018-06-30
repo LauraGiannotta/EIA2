049091393e6ca430aa2cedf5687c4292f07e88e1
lagia <lagia@LAPTOP-8U4NVVVF>
2018-06-30 23:58:43 +0200

    push

--- Aufgabe11/Aufgabe11.1/Animation.js
+++ Aufgabe11/Aufgabe11.1/Animation.js
@@ -3,34 +3,45 @@ var Aufgabe11_1;
     window.addEventListener("load", init);
     let stars = [];
     let n = 30;
+    //let rects: Rect[] = [];
     function init(_event) {
         let canvas = document.getElementsByTagName("canvas")[0];
         Aufgabe11_1.crc2 = canvas.getContext("2d");
         console.log(Aufgabe11_1.crc2);
         canvas.addEventListener("click", insertNewObject);
         for (let i = 0; i < n; i++) {
-            let star = new Aufgabe11_1.DavidStar("#c00000");
+            let star = new Aufgabe11_1.DavidStar("#cc0000");
             stars.push(star);
-            let rect = new Aufgabe11_1.Rect("#fff900");
+            let rect = new Aufgabe11_1.Rect("#ff9900");
             stars.push(rect);
+            let triangle = new Aufgabe11_1.Triangle("#7CCD7C");
+            stars.push(triangle);
         }
         animate();
     }
     function insertNewObject(_event) {
+        // let star: DavidStar = new DavidStar("#ffff00");
+        //stars.push(star);
         let _x = _event.pageX;
         let _y = _event.pageY;
         let zufall = Math.random() * 13;
         if (zufall < 4) {
-            let star = new Aufgabe11_1.DavidStar("#E6E6FA");
+            let star = new Aufgabe11_1.DavidStar("#7CCD7C");
             star.x = _x;
             star.y = _y;
             stars.push(star);
         }
         else if (zufall < 8) {
-            let rect = new Aufgabe11_1.Rect("#FFF0F5");
+            let rect = new Aufgabe11_1.Rect("#EEE0E5");
             rect.x = _x;
             rect.y = _y;
             stars.push(rect);
+        }
+        else {
+            let triangle = new Aufgabe11_1.Triangle("#CDB7B5");
+            triangle.x = _x;
+            triangle.y = _y;
+            stars.push(triangle);
         }
     }
     function animate() {
