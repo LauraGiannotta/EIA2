0c06e47c2d277ff200eaeeebe757dfd9fe61032d
lagia <lagia@LAPTOP-8U4NVVVF.hfu-open.fu.hs-furtwangen.de>
2018-07-22 14:04:47 +0200

    d

--- Abschlussaufgabe/footer.js
+++ Abschlussaufgabe/footer.js
@@ -1,1 +1,23 @@
+var HungryCat;
+(function (HungryCat) {
+    class Food extends HungryCat.MovingObjects {
+        constructor(_x, _y, _color) {
+            super(_x, _y, _color);
+        }
+        move() {
+            this.y += 10;
+            if (this.y > 630) {
+                this.y = 630;
+            }
+        }
+        draw() {
+            HungryCat.crc2.beginPath();
+            HungryCat.crc2.arc(this.x, this.y, 3, 0, 5 * Math.PI);
+            HungryCat.crc2.closePath();
+            HungryCat.crc2.fillStyle = "rgb(80, 20, 20)";
+            HungryCat.crc2.fill();
+        }
+    }
+    HungryCat.Food = Food;
+})(HungryCat || (HungryCat = {}));
 //# sourceMappingURL=footer.js.map
--- Abschlussaufgabe/maus.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=maus.js.map
